85f59e5dba40387c2b1b78b3e017930bd267dd0a
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-09-18 14:54:48 +0900

    first commit

---
+++ js/example.js
@@ -0,0 +1,8 @@
+document.write("Hello, World!\"헬로우 월드\""); // "Hello, World!"를 웹 페이지에 출력
+
+var str1 = "kor";
+var str2 = "jap";
+var num = 8;
+var num2 = Number("100"); // 문자 100 -> 숫자 100으로 저장
+var tag1 = "<h1> String </h1>"; // <h1> 태그를 변수에 저장
+var bool1 = Boolean(null); // 0과 null을 제외한 모든 값은 true
